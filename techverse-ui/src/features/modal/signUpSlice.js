43673b8361c8f0c0f9862e98937a9cf2fa0ec338
sachin <sachin.sahu@cloudera.com>
2023-07-29 18:26:32 +0530

    Added sign up page

---
+++ techverse-ui/src/features/modal/signUpSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+const initialState = {
+  isOpen: false,
+  isLoading: false,
+  profile: {},
+};
+
+export const registerUser = createAsyncThunk(
+  "signup/registerUser",
+  async (payload, thunkAPI) => {
+    try {
+      const resp = await axios(url);
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue("Something went wrong");
+    }
+  }
+);
+
+const signUpSlice = createSlice({
+  name: "signUp",
+  initialState,
+  reducers: {
+    openSignUpModal: (state) => {
+      state.isOpen = true;
+    },
+    closeSignUpModal: (state) => {
+      state.isOpen = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.profile = action.payload;
+      state.isOpen = false;
+    });
+    builder.addCase(registerUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      state.isLoading = false;
+      console.log(action);
+    });
+  },
+});
+
+export const { openSignUpModal, closeSignUpModal } = signUpSlice.actions;
+export default signUpSlice.reducer;
---
+++ techverse-ui/src/pages/SignUp.jsx
@@ -0,0 +1,255 @@
+import {
+  Form,
+  Input,
+  InputNumber,
+  DatePicker,
+  Select,
+  Button,
+  Space,
+  Modal,
+} from "antd";
+import { PlusOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import React from "react";
+import { styled } from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { closeSignUpModal, registerUser } from "../features/modal/signUpSlice";
+
+const options = [
+  {
+    value: "python",
+    label: "Python",
+  },
+  {
+    value: "javascript",
+    label: "Javascript",
+  },
+  {
+    value: "java",
+    label: "Java",
+  },
+  {
+    value: "angular",
+    label: "Angular",
+  },
+  {
+    value: "nodejs",
+    label: "NodeJs",
+  },
+  {
+    value: "react",
+    label: "React",
+  },
+  {
+    value: "vue",
+    label: "Vue",
+  },
+  {
+    value: "aws",
+    label: "AWS",
+  },
+  {
+    value: "kafka",
+    label: "Kafka",
+  },
+];
+
+const StyledModal = styled(Modal)`
+.ant-modal-body {
+  max-height: 400px; /* Adjust the height as per your requirement */
+  overflow-y: auto;
+  padding-right: 16px; 
+}`;
+
+const SignUpForm = () => {
+  const { isOpen, isLoading } = useSelector((store) => store.signUp);
+  const dispatch = useDispatch();
+  const [form] = Form.useForm();
+
+  const closeModalContainer = () => {
+    if (isLoading) {
+      return;
+    }
+    form.resetFields();
+    dispatch(closeSignUpModal());
+  };
+
+  const handleOk = async () => {
+    if (isLoading) {
+      return;
+    }
+    try {
+      const values = await form.validateFields();
+      console.log(values);
+      dispatch(registerUser(values));
+    } catch (errorInfo) {
+      console.log("Validation Failed:", errorInfo);
+    }
+  };
+
+  return (
+    <StyledModal
+      open={isOpen}
+      okText="Sign up"
+      cancelText="Cancel"
+      closeIcon={null}
+      maskClosable={false}
+      onCancel={closeModalContainer}
+      onOk={handleOk}
+      confirmLoading={isLoading}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="signupForm"
+        initialValues={{ modifier: "public" }}
+      >
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your First Name!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Last Name!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Space style={{ display: "flex" }} align="start">
+          <Form.Item
+            label="Mobile"
+            name="mobile"
+            rules={[
+              { required: true, message: "Please enter your mobile number" },
+              {
+                pattern: /^(\+\d{1,3}[- ]?)?\d{10}$/,
+                message: "Please enter a valid mobile number",
+              },
+            ]}
+            style={{ flex: 1, marginRight: 8 }}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Date of Birth"
+            name="dob"
+            rules={[
+              { required: true, message: "Please select your date of birth" },
+            ]}
+            style={{ flex: 1 }}
+          >
+            <DatePicker />
+          </Form.Item>
+        </Space>
+
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Please enter your email address" },
+            { type: "email", message: "Please enter a valid email address" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Job Role"
+          name="jobRole"
+          rules={[{ required: true, message: "Please enter your job role" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label={
+            <span
+              style={{
+                whiteSpace: "normal",
+                width: "100%",
+                display: "inline-block",
+              }}
+            >
+              Experience (in Years)
+            </span>
+          }
+          name="experience"
+        >
+          <InputNumber min={0} style={{ width: "100%" }} />
+        </Form.Item>
+
+        <Form.Item label="Technologies" name="technologies">
+          <Select
+            mode="tags"
+            style={{ width: "100%" }}
+            placeholder="Select or enter technologies"
+            options={options}
+          />
+        </Form.Item>
+
+        <Form.Item label="Projects">
+          <Form.List name="projects">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map((field) => (
+                  <Space
+                    key={field.key}
+                    style={{ display: "flex", marginBottom: 8 }}
+                    align="start"
+                  >
+                    <Form.Item
+                      {...field}
+                      name={[field.name, "name"]}
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please enter project name",
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Project Name" />
+                    </Form.Item>
+
+                    <Form.Item {...field} name={[field.name, "details"]}>
+                      <Input.TextArea rows={2} placeholder="Project Details" />
+                    </Form.Item>
+
+                    {fields.length > 1 ? (
+                      <MinusCircleOutlined onClick={() => remove(field.name)} />
+                    ) : null}
+                  </Space>
+                ))}
+
+                <Form.Item>
+                  <Button
+                    type="dashed"
+                    onClick={() => add()}
+                    block
+                    icon={<PlusOutlined />}
+                  >
+                    Add Project
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+        </Form.Item>
+      </Form>
+    </StyledModal>
+  );
+};
+
+export default SignUpForm;
